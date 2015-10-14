e347751fbcc23a89be2e46eb29bc5158d6888832
Dylan Kuper <dylan.kuper@gmail.com>
2015-10-14 16:21:00 -0700

    adding unfinished birthdaylistviewer code

--- src/BirthdayListViewer.java
+++ src/BirthdayListViewer.java
@@ -1,8 +1,13 @@
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 
 // TODO implement this class such that the birthdays
 // in the list are shown on the JFrame in some fashion.
 public class BirthdayListViewer extends JFrame {
+	
+	JList thrift = new JList();
+	DefaultListModel j = new DefaultListModel();
 	
 	public static void main(String[] args) {
 		
